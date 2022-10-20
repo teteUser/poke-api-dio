814a27f4116f1d6822c5babad177b5609fb58394
Tennison Capra <tennison_capra@hotmail.com>
2022-10-19 23:32:14 -0300

    adding a button to show more pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,28 @@
-const ol = document.querySelector('.pokemons');
+const pokemonsOL = document.querySelector('.pokemons');
+const loadMoreButton = document.querySelector('#loadMore');
+const limit = 5;
+let offset = 0;
 
-function convertPokemonToLi(pokemon){
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>
-`
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemonList = []) => pokemonsOL.innerHTML += pokemonList.map((pokemon) => {
+        return `<li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>`
+    }).join(''))
+    .catch((error) => console.log(error))
 }
 
-pokeApi.getPokemons().then((pokemonList = []) => ol.innerHTML += pokemonList.map(convertPokemonToLi).join(''))
-.catch((error) => console.log(error))
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonItens(offset, limit);
+})
